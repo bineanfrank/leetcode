8cff49b52811e2e0f35fbe3a2bc2a5b6ef9a7ce6
Harlan1994 <1041099438@qq.com>
2019-04-14 12:13:04 +0800

    binary

---
+++ src/binary_points/L27_移除元素.java
@@ -0,0 +1,32 @@
+package binary_points;
+
+/**
+ * Created by Harlan1994 on 2018/12/21.
+ */
+public class L27_移除元素 {
+
+    public static void main(String[] args) {
+        L27_移除元素 l27_移除元素 = new L27_移除元素();
+
+        int[] ints = new int[]{1, 2, 2, 3, 4, 5, 6};
+        int result = l27_移除元素.removeElement(ints, 2);
+        System.out.println(result);
+    }
+
+    public int removeElement(int[] nums, int val) {
+        int i = -1, j = 0;
+        while (j < nums.length && nums[j] != val) {
+            j++;
+        }
+        if (j == nums.length) return j;
+        i = j + 1;
+        while (i < nums.length) {
+            if (nums[i] != val) {
+                nums[j++] = nums[i++];
+            } else {
+                i++;
+            }
+        }
+        return j;
+    }
+}
