0421375264fb1a28eed5542fc6d2fb2dae4cd955
bineanfrank <bineanfrank@gmail.com>
2021-09-06 15:15:11 +0800

    添加：L704_二分查找

---
+++ src/questionbank/L704_二分查找.java
@@ -0,0 +1,20 @@
+package questionbank;
+
+public class L704_二分查找 {
+
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (right + left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
